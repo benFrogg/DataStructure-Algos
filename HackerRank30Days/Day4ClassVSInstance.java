c9702a808199fc1099f1d474fb056b9361627715
benFrogg <87656600+benFrogg@users.noreply.github.com>
2021-12-05 17:40:58 +0800

    Update Day4ClassVSInstance.java

--- HackerRank30Days/Day4ClassVSInstance.java
+++ HackerRank30Days/Day4ClassVSInstance.java
@@ -1,69 +1,3 @@
-/* 
-Objective
-
-In this challenge, we're going to learn about the difference between a class and an instance; 
-because this is an Object Oriented concept, it's only enabled in certain languages. 
-Check out the Tutorial tab for learning materials and an instructional video!
-
-Task
-
-Write a Person class with an instance variable, age, and a constructor that takes an intger,
-initialAge, as a parameter. The constructor must assign initialAge to age after confirming
-the argument passes as initialAge is not negative; if a negative argument is passed as
-initialAge, the constructor should set age to 0 and print "Age is not valid", setting age
-to 0. In addition, you must write the following instance methods:
-    1. yearPasses() should increase the age instance variable by 1.
-    2. amIOld() should perform the following conditional actions:
-        - If age < 13, print "You are young."
-        - If age >= 13 and age < 18, print "You are a teenage."
-        - Otherwise, print "You are old"
-
-To help you learn by example and complete this challenge, much of the code is provided for you, 
-but you'll be writing everything in the future. 
-The code that creates each instance of your Person class is in the main method. 
-Don't worry if you don't understand it all quite yet!
-
-Note: Do not remove or alter the stub code in the editor.
-
-Input Format
-
-Input is handled for you by stub code in the editor.
-The first line contains an integer, T (the number of test cases), and the T subsequent
-lines each contain an integer denoting the age of a Person instance.
-
-Constraints 
-    - 1 <= T <= 4
-    - -5 <= age <= 30
-
-Output Format
-
-Complete the method definitions provided in the editor so they meet the specifications outlined above; 
-the code to test your work is already in the editor. If your methods are implemented correctly, 
-each test case will print 2 or 3 lines (depending on whether or not a valid initialAge was passed to the constructor).
-
-
-Sample Input
-4
--1
-10
-16
-18
-
-Sample Output
-
-Age is not valid, setting age to 0.
-You are young.
-You are young.
-
-You are young.
-You are a teenager.
-
-You are a teenager.
-You are old.
-
-You are old.
-You are old
-*/
 
 import java.util.*;
 
@@ -71,7 +5,6 @@ public class Day4ClassVSInstance {
     private int age;
 
     public Day4ClassVSInstance(int initialAge) {
-        // Add some more code to run some checks on initialAge
         age = initialAge;
 
         if (age < 0) {
@@ -81,7 +14,6 @@ public class Day4ClassVSInstance {
     }
 
     public void amIOld() {
-        // Write code determining if this person's age is old and print the correct statement:
         if (age < 13) {
             System.out.println("You are young.");
         } else if (age >= 13 && age < 18) {
@@ -92,7 +24,6 @@ public class Day4ClassVSInstance {
     }
 
     public void yearPasses() {
-        // Increment this person's age.
         age += 1;
     }
 
