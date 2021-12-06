2c54a5e1c5922739db575973542bfb3663db1bf7
benFrogg <87656600+benFrogg@users.noreply.github.com>
2021-12-07 01:41:45 +0800

    Update Day11Arrays2D.java

--- HackerRank30Days/Day11Arrays2D.java
+++ HackerRank30Days/Day11Arrays2D.java
@@ -27,9 +27,9 @@ public class Day11Arrays2D {
         
         int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i <= 3; i++)
         {
-            for (int j = 0; j < 4; j++)
+            for (int j = 0; j <= 3; j++)
             {
                 int sum = 0;
 
