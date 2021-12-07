2ed464b56314a829621cb1bf90fc97d826b2db87
benFrogg <87656600+benFrogg@users.noreply.github.com>
2021-12-07 19:58:15 +0800

    Update Day12Inheritance.java

--- HackerRank30Days/Day12Inheritance.java
+++ HackerRank30Days/Day12Inheritance.java
@@ -1,59 +1,3 @@
-/*
-Task 
-You are given two classes, Person and Student, where Person is the base class and Student
-is the derived class. Completed code for Person and a declaration for Student are provided
-for you in the editor. Observe that Student inherits all the properties of Person.
-
-Complete the Student class by writing the following:
-    - A Student class constructor, which has 4 parameters:
-        1. A string, firstName.
-        2. A string, lastName.
-        3. An integer, idNumber.
-        4. An integer array (or vector) of test scores, scores.
-    - A char calculate() method that calculates a Student object's average and returns the
-        grade character representative of their calculated average:
-
-            Grading Scale
-        Letter |   Average (a)
-        -------------------------
-           O   | 90 <= a <= 100
-           E   | 80 <= a < 90
-           A   | 70 <= a < 80
-           P   | 55 <= a < 70
-           D   | 40 <= a < 55
-           T   |    a < 40
-
-Input Format
-The locked stub code in the editor reads the input and calls the Student class constructor
-with the necessary arguments. It also calls the calculate method which takes no arguments.
-The first line contains firstName, lastName, and idNumber, separated by a space. The second
-line contains the number of test scores. The third line of sapce-separated integers describes
-scores.
-
-Constraints
-    - 1 <= length of firstName, length of lastName <= 10
-    - length of idNumber = 7
-    - 0 <= score <= 100
-
-Output Format
-Output is handled by the locked stub code. Your output will be correct if your Student class
-constructor and calculate() method are properly implemented.
-
-Sample Input
-Heraldo Memelli 8135627
-2
-100 80
-
-Sample Output
-Name: Memelli, Heraldo
-ID: 8135627
-Grade: O
-
-Explanation 
-This student has 2 scores to average: 100 and 80. The student's average grade is (100+80)/2 = 90.
-An average grade of 90 corresponds to the letter grade O, so the calculate() method should return
-the character 'O'.
-*/
 
 import java.util.*;
 
@@ -62,14 +6,12 @@ class Person {
 	protected String lastName;
 	protected int idNumber;
 	
-	// Constructor
 	Person(String firstName, String lastName, int identification){
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = identification;
 	}
 	
-	// Print person data
 	public void printPerson(){
 		 System.out.println(
 				"Name: " + lastName + ", " + firstName 
@@ -81,26 +23,11 @@ class Person {
 class Student extends Person{
 	private int[] testScores;
     
-
-    /*	
-    *   Class Constructor
-    *   
-    *   @param firstName - A string denoting the Person's first name.
-    *   @param lastName - A string denoting the Person's last name.
-    *   @param id - An integer denoting the Person's ID number.
-    *   @param scores - An array of integers denoting the Person's test scores.
-    */
-    // Write your constructor here
     public Student(String firstName, String lastName, int id, int[] scores){
         super(firstName, lastName, id);
         this.testScores = scores;
     }
 
-    /*	
-    *   Method Name: calculate
-    *   @return A character denoting the grade.
-    */
-    // Write your method here
     public char calculate(){
         int totalScore = 0;
         char grade = 'T';
